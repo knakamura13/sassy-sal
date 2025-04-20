f8e3628732735f6e46f0f2a5468bb561e163178f
Kyle Nakamura <knakamura13dev@gmail.com>
2025-04-20 03:49:44 -0700

    chore: remove sanity studio config dir

--- sanity/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import category from './category';
-import galleryImage from './galleryImage';
-import about from './about';
-import contact from './contact';
-import pricing from './pricing';
-
-export default [category, galleryImage, about, contact, pricing];
